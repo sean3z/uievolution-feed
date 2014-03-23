33ec8ee36d9d2cb6a790155d5acb8e4028868660
Sean Wragg <seanwragg@gmail.com>
2014-03-23 08:20:06 -0400

    moving feed.js down towards end of body

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<script type="text/javascript" src="assets/js/zepto.min.js"></script>
-		<script type="text/javascript" src="assets/js/feed.js"></script>
 		<title>PWFantasy Feed</title>
 	</head>
 	<body>
 		<ul id="content"></ul>
+		<script type="text/javascript" src="assets/js/feed.js"></script>
 	</body>
 </html>
