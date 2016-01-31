992a7cc272a0a4271c39cef91158b18e0d7ffcd3
Sean Wragg <seanwragg@gmail.com>
2016-01-31 18:36:57 -0500

    updates

--- assets/js/feed.js
+++ assets/js/feed.js
@@ -1,48 +1,72 @@
 // unfortunately js on windows phone 8 ie10 is limited
-var $parent = $('ul#content'), loading = false, ga = window.ga || function(){};
+var $parent = $('ul#feed'), articles = [], _loading = false, ga = window.ga || function(){};
 
 function initializeFeed(data) {
 	$parent.empty();
-	var items = data.item;
+	articles = [];
+	var items = data.item || [];
 	if (typeof items != 'undefined' && items.length > 0) {
 		for(var i = 0; i <= items.length-1; i++) {
-			addItem(items[i]);
+			var item = items[i];
+			$parent.append('<li><a href="'+ item.link +'" data-article="'+ i +'">'+ item.title +'</a><br /><span class="date">'+ item.pubDate +'</span></li>');
+			articles.push(item);
 		};
 	}
 
-	$('#content a').bind('click', function(e) {
+	$('#feed a').bind('click', function(e) {
 		e.preventDefault();
+
+		if (this.href == $('#article').data('href')) {
+			$('#article, #feed').toggle();
+			return;
+		}
 
 		var site = $('body').attr('class');
 		ga('send', 'pageview', {
 			'page': fragmentURL(site, this.href),
 			'title': this.text
 		});
-		fetchArticle(site, this.href, function() {
-			$('#article, #content').toggle();
-		});
+
+		initializeArticle(site, this.href, $(this).data('article'));
+
 		return false;
 	});
 }
 
-function initializeArticle(data) {
-	var data = data || {};
-	if (typeof data.article != 'undefined') {
-		var a = '<a href="/feed/" class="back">Back</a>&nbsp;|&nbsp;<a href="#" class="reload">Reload</a><br /><br />';
-		$('#article').data('href', data.url).html(a + data.article + a);
+function loading(state) {
+	if (state) {
+		$('#hamburger').addClass('loading');
+	} else {
+		$('#hamburger').removeClass('loading');
+	}
+	_loading = state;
+}
+
+function initializeArticle(site, href, articleId) {
+	var data = articles[articleId];
+	if (typeof data != 'undefined') {
+		window.scrollTo(0,0);
+		//var a = '<a href="/feed/" class="back">Back</a>&nbsp;|&nbsp;<a href="#" class="reload">Reload</a><br /><br />';
+		var a = '<a href="/feed/" class="back">Back</a><br /><br />';
+		$('#article').data('href', data.link).html(a + data.description +'<br /><br />'+ a);
+
+		$('#article').find('img').width('auto');
 
 		$('#article a.reload').bind('click', function (e) {
 			e.preventDefault();
-			fetchArticle($('body').attr('class'), $('#article').data('href'));
+			$('#article').html('');
+			//fetchArticle($('body').attr('class'), $('#article').data('href'));
 			return false;
 		});
 
 		$('#article a.back').bind('click', function (e) {
 			e.preventDefault();
-			$('#article, #content').toggle();
+			$('#article, #feed').toggle();
 			return false;
 		});
 	}
+
+	$('#article, #feed').toggle();
 }
 
 function fragmentURL(site, url) {
@@ -55,18 +79,10 @@ function fragmentURL(site, url) {
 	return '/'+ site + url;
 }
 
-function addItem(item) {
-	$parent.append('<li><a href="'+ item.link +'">'+ shorten(item.title) +'</a><br /><span class="date">'+ item.pubDate +'</span></li>');
-}
-
-function shorten(title) {
-	return (title.length > 50 ? title.substring(0, 49) +'&hellip;' : title)
-}
-
 function fetchFeed(site, callback) {
 	var site = site || 'dotnet';
-	if (loading) return;
-	loading = true;
+	if (_loading) return;
+	loading(true);
 	$.ajax({
 		url: 'api/feed.php',
 		dataType: 'json',
@@ -74,52 +90,43 @@ function fetchFeed(site, callback) {
 		success: function(data) {
 			initializeFeed(data);
 			if (typeof callback != 'undefined') callback();
-			loading = false;
+			loading(false);
 		},
 		error: function (data) {
-			loading = false;
+			loading(false);
 		}
 	});
 }
 
-function fetchArticle(site, url, callback) {
-	var site = site || 'dotnet';
-	if (loading) return;
-	loading = true;
-	$.ajax({
-		url: 'api/article.php',
-		dataType: 'json',
-		data: {type: site, article: url},
-		success: function(data) {
-			initializeArticle(data);
-			if (typeof callback != 'undefined') callback();
-			loading = false;
-		},
-		error: function (data) {
-			loading = false;
-			window.location = this.href;
-		}
-	});
+function breakpoint() {
+	var w = $(window).width(),
+		breakpoint = 'small';
+
+	if (w >= 400) {
+		breakpoint = 'x-large';
+	} else if (w >= 380) {
+		breakpoint = 'large';
+	} else if (w >= 360) {
+		breakpoint = 'medium';
+	}
+
+	$('html').addClass(breakpoint);
 }
 
 $(document).ready(function() {
+	breakpoint();
 	fetchFeed();
 
-	$('.hamburger a').bind('click', function(e) {
+	$('#hamburger').bind('click', function(e) {
 		e.preventDefault();
 		var $body = $('body'), site = $body.attr('class');
 		site = (site == 'dotnet') ? 'pwtorch' : 'dotnet';
 		ga('send', 'event', 'switchto', 'click', site)
 		fetchFeed(site, function() {
 			$body.removeClass('dotnet pwtorch').addClass(site);
-			$('#content').show();
+			$('#feed').show();
 			$('#article').hide();
-			$('.logo img').toggle();
 		});
 		return false;
 	});
-});
-
-$(window).bind('popstate', function (event) {
-	$('#article, #content').toggle();
 });
